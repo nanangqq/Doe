7f3ecec3017847746cb58ac8f7b919fb8aa3472e
nanangqq <gingggg@gmail.com>
2023-07-01 11:41:41 +0900

    렌더링 함수 생성 코드를 컴포넌트 외부로 분리하라

--- ssd/src/app.tsx
+++ ssd/src/app.tsx
@@ -2,35 +2,30 @@ import React, { useEffect } from 'react'
 import { fbApp } from './fb'
 import TestPage from './pages/test'
 import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
 } from 'react-router-dom'
 import ErrorPage from './pages/error'
 import RootPage from './pages/root'
-import IframePage from './pages/iframe'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: 'test',
-    element: <TestPage />,
-  },
-  {
-    path: 'iframe',
-    element: <IframePage />,
-  },
-  {
-    path: 'test/bb',
-    element: <div>hey</div>,
-  },
+    {
+        path: '/',
+        element: <RootPage />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: 'test',
+        element: <TestPage />,
+    },
+    {
+        path: 'test/bb',
+        element: <div>hey</div>,
+    },
 ])
 
 export default () => {
-  return <RouterProvider router={router} />
+    return <RouterProvider router={router} />
 }
--- ssd/src/components/view.tsx
+++ ssd/src/components/view.tsx
@@ -7,177 +7,188 @@ import { useWebGPU } from '../webgpu/helper'
 import testdata from '../geo/testdata'
 import { createPolygonShader } from '../webgpu/shaders'
 
-export default function ViewComponent({
-  width,
-  height,
-  unit = 'mm',
-}: {
-  width: string
-  height: string
-  unit?: 'mm' | 'inch'
-}) {
-  const canvasId = useMemo(() => `view-canvas-${v4()}`, [])
-  const [isMouseOnCanvas, setIsMouseOnCanvas] = useState(false)
-  const [mousePosition, setMousePosition] = useState<[number, number]>([0, 0])
+const createRenderer = async (canvasId: string) => {
+    const { canvas, context, adapter, device } = await useWebGPU(canvasId)
+    // console.log(canvas.width, canvas.height)
+    // canvas.addEventListener('mousemove', (e) => {
+    //   setIsMouseOnCanvas(true)
+    //   const x = (e.offsetX - canvas.width / 2) / (canvas.width / 2)
+    //   const y = (-e.offsetY + canvas.height / 2) / (canvas.height / 2)
+    //   // console.log(x, y)
 
-  useEffect(() => {
-    console.log(mousePosition)
-  }, mousePosition)
+    // })
+    const format = navigator.gpu.getPreferredCanvasFormat()
 
-  const init = () => {
-    useWebGPU(canvasId).then((res) => {
-      const { canvas, context, adapter, device } = res
-      // console.log(canvas.width, canvas.height)
-      // canvas.addEventListener('mousemove', (e) => {
-      //   setIsMouseOnCanvas(true)
-      //   const x = (e.offsetX - canvas.width / 2) / (canvas.width / 2)
-      //   const y = (-e.offsetY + canvas.height / 2) / (canvas.height / 2)
-      //   // console.log(x, y)
-
-      // })
-      const format = navigator.gpu.getPreferredCanvasFormat()
-
-      context.configure({
+    context.configure({
         device: device,
         format: format,
-      })
+    })
 
-      const fc = testdata()
-      const pol = fc.features[1] as Feature<Polygon>
-      const pt = fc.features[0] as Feature<Point>
+    const fc = testdata()
+    const pol = fc.features[1] as Feature<Polygon>
+    const pt = fc.features[0] as Feature<Point>
 
-      const vertices = new Float32Array(pol.geometry.coordinates[0].flat())
-      // console.log(vertices)
-      const polVertsBuffer = device.createBuffer({
+    const vertices = new Float32Array(pol.geometry.coordinates[0].flat())
+    // console.log(vertices)
+    const polVertsBuffer = device.createBuffer({
         size: vertices.byteLength,
         usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-      })
+    })
 
-      device.queue.writeBuffer(polVertsBuffer, 0, vertices)
+    device.queue.writeBuffer(polVertsBuffer, 0, vertices)
 
-      const vertexBufferLayout: GPUVertexBufferLayout = {
+    const vertexBufferLayout: GPUVertexBufferLayout = {
         arrayStride: 8,
         attributes: [
-          {
-            format: 'float32x2',
-            offset: 0,
-            shaderLocation: 0, // Position, see vertex shader
-          },
+            {
+                format: 'float32x2',
+                offset: 0,
+                shaderLocation: 0, // Position, see vertex shader
+            },
         ],
-      }
+    }
 
-      const bindGroupLayout = device.createBindGroupLayout({
+    const bindGroupLayout = device.createBindGroupLayout({
         label: 'pol Bind Group Layout',
         entries: [
-          {
-            binding: 0,
-            visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
-            buffer: {}, // uniform buffer
-          },
+            {
+                binding: 0,
+                visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+                buffer: {}, // uniform buffer
+            },
         ],
-      })
+    })
 
-      // Create a uniform buffer that describes the grid.
-      const uniformArray = new Float32Array([0, 0])
-      // console.log(uniformArray)
-      const uniformBuffer = device.createBuffer({
+    // Create a uniform buffer that describes the grid.
+    const uniformArray = new Float32Array([0, 0])
+    // console.log(uniformArray)
+    const uniformBuffer = device.createBuffer({
         label: 'Grid Uniforms',
         size: uniformArray.byteLength,
         usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-      })
-      // console.log(uniformBuffer)
-      device.queue.writeBuffer(uniformBuffer, 0, uniformArray)
-      const bindGroup = device.createBindGroup({
+    })
+    // console.log(uniformBuffer)
+    device.queue.writeBuffer(uniformBuffer, 0, uniformArray)
+    const bindGroup = device.createBindGroup({
         label: 'Cell renderer bind group A',
         layout: bindGroupLayout,
         entries: [
-          {
-            binding: 0,
-            resource: { buffer: uniformBuffer },
-          },
+            {
+                binding: 0,
+                resource: { buffer: uniformBuffer },
+            },
         ],
-      })
-      const pipelineLayout = device.createPipelineLayout({
+    })
+    const pipelineLayout = device.createPipelineLayout({
         label: 'pol Pipeline Layout',
         bindGroupLayouts: [bindGroupLayout],
-      })
+    })
 
-      const polShaderModule = device.createShaderModule(createPolygonShader())
+    const polShaderModule = device.createShaderModule(createPolygonShader())
 
-      const polPipeline = device.createRenderPipeline({
+    const polPipeline = device.createRenderPipeline({
         label: 'pol pipeline',
         layout: pipelineLayout,
         vertex: {
-          module: polShaderModule,
-          entryPoint: 'vertexMain',
-          buffers: [vertexBufferLayout],
+            module: polShaderModule,
+            entryPoint: 'vertexMain',
+            buffers: [vertexBufferLayout],
         },
         fragment: {
-          module: polShaderModule,
-          entryPoint: 'fragmentMain',
-          targets: [
-            {
-              format,
-            },
-          ],
+            module: polShaderModule,
+            entryPoint: 'fragmentMain',
+            targets: [
+                {
+                    format,
+                },
+            ],
         },
         primitive: {
-          topology: 'line-strip',
+            topology: 'line-strip',
         },
-      })
-
-      const encoder = device.createCommandEncoder()
-      const pass = encoder.beginRenderPass({
-        colorAttachments: [
-          {
-            view: context.getCurrentTexture().createView(),
-            loadOp: 'clear',
-            clearValue: { r: 0.5, g: 0.5, b: 0.5, a: 1 }, // New line
-            storeOp: 'store',
-          },
-        ],
-      })
-
-      pass.setBindGroup(0, bindGroup)
-      pass.setPipeline(polPipeline)
-      pass.setVertexBuffer(0, polVertsBuffer)
-      pass.draw(5)
-
-      pass.end()
-      device.queue.submit([encoder.finish()])
     })
-  }
-  useEffect(init, [])
 
-  return (
-    <DefaultViewContainer
-      className="view-container"
-      width={width}
-      height={height}
-    >
-      <canvas
-        // 캔버스는 width, height를 단순 숫자값으로 받아서 px단위 크기로 사이즈가 정해짐.
-        // style로 사이즈 넣으면 기본사이즈에서 뻥튀기 되는 현상 나타남.
-        // 숫자 뒤에 %, px 등 단위 넣어져도 무시하고 숫자값으로 받아들임.
-        id={canvasId}
-        width={width}
-        height={height}
-        onMouseMove={(e) => {
-          setIsMouseOnCanvas(true)
-          const x =
-            (e.nativeEvent.offsetX - Number(width.replace('px', '')) / 2) /
-            (Number(width.replace('px', '')) / 2)
-          const y =
-            (-e.nativeEvent.offsetY + Number(height.replace('px', '')) / 2) /
-            (Number(height.replace('px', '')) / 2)
-          setMousePosition([x, y])
-        }}
-      />
-    </DefaultViewContainer>
-  )
+    const renderer = () => {
+        const encoder = device.createCommandEncoder()
+        const pass = encoder.beginRenderPass({
+            colorAttachments: [
+                {
+                    view: context.getCurrentTexture().createView(),
+                    loadOp: 'clear',
+                    clearValue: { r: 0.5, g: 0.5, b: 0.5, a: 1 }, // New line
+                    storeOp: 'store',
+                },
+            ],
+        })
+
+        pass.setBindGroup(0, bindGroup)
+        pass.setPipeline(polPipeline)
+        pass.setVertexBuffer(0, polVertsBuffer)
+        pass.draw(5)
+
+        pass.end()
+        device.queue.submit([encoder.finish()])
+    }
+
+    return renderer
+}
+
+export default function ViewComponent({
+    width,
+    height,
+    unit = 'mm',
+}: {
+    width: string
+    height: string
+    unit?: 'mm' | 'inch'
+}) {
+    const canvasId = useMemo(() => `view-canvas-${v4()}`, [])
+    const [isMouseOnCanvas, setIsMouseOnCanvas] = useState(false)
+    const [renderer, setRenderer] = useState<() => void>(() => () => {})
+    const [mousePosition, setMousePosition] = useState<[number, number]>([0, 0])
+
+    const init = () => {
+        createRenderer(canvasId).then((renderer) => setRenderer(() => renderer))
+    }
+    useEffect(init, [])
+
+    useEffect(() => {
+        console.log(mousePosition)
+        console.log(renderer)
+        renderer()
+    }, [mousePosition, renderer])
+
+    return (
+        <DefaultViewContainer
+            className="view-container"
+            width={width}
+            height={height}
+        >
+            <canvas
+                // 캔버스는 width, height를 단순 숫자값으로 받아서 px단위 크기로 사이즈가 정해짐.
+                // style로 사이즈 넣으면 기본사이즈에서 뻥튀기 되는 현상 나타남.
+                // 숫자 뒤에 %, px 등 단위 넣어져도 무시하고 숫자값으로 받아들임.
+                id={canvasId}
+                width={width}
+                height={height}
+                onMouseMove={(e) => {
+                    setIsMouseOnCanvas(true)
+                    const x =
+                        (e.nativeEvent.offsetX -
+                            Number(width.replace('px', '')) / 2) /
+                        (Number(width.replace('px', '')) / 2)
+                    const y =
+                        (-e.nativeEvent.offsetY +
+                            Number(height.replace('px', '')) / 2) /
+                        (Number(height.replace('px', '')) / 2)
+                    setMousePosition([x, y])
+                }}
+            />
+        </DefaultViewContainer>
+    )
 }
 
 const DefaultViewContainer = styled.div<{ width: string; height: string }>`
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
 `
